76b147cfa210a0dad8a4c8c60051b4bd3dfa93f8
SotaImahayshi <104618028+SotaImahayshi@users.noreply.github.com>
2022-05-09 16:38:21 +0900

    Update FizzBuzz.py

--- template/Q1_FizzBuzz/FizzBuzz.py
+++ template/Q1_FizzBuzz/FizzBuzz.py
@@ -2,8 +2,14 @@ for num in range(1, 101):
     string = ''
 
     # ここから記述
-
-
+    if num % 15 == 0:            #15の倍数の場合（数字の代わりにFizzBuzzと出力する）
+        string = "FizzBuzz"
+    elif num % 5 == 0:           #5の倍数の場合（数字の代わりにBuzzと出力する）
+        string = "Buzz"
+    elif num % 3 == 0:           #3の倍数の場合（数字の代わりにFizzと出力する）
+        string = "Fizz"
+    else:                        #3,5,15の倍数以外の場合（そのまま数字を出力する）
+        string = num
     # ここまで記述
 
     print(string)
